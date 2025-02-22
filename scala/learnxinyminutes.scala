814ae5c30ba26b4c1cb45bcbe817936e477cf0f2
BrunoCasarotto <bruno.casarotto@hotmail.com>
2025-02-22 10:46:08 -0300

    chore: anonymous function

--- scala/learnxinyminutes.scala
+++ scala/learnxinyminutes.scala
@@ -1,14 +1,31 @@
-object HelloWorld {
+object LearnScala {
+
     def main(args: Array[String]): Unit = {
         println("Start")
         println("--------------------")
-        
+
         val sum = sumOfSquares(3, 4)
         println(sum)
         println("--------------------")
 
         val sumShort = sumOfSquaresShort(5, 5)
         println(sumShort)
+        println("--------------------")
+
+        val anonymous = anonymousFunction(6)
+        println(anonymous)
+        println("--------------------")
+
+        val sq: Int => Int = x => x * x
+        println(sq(10))
+        println("--------------------")
+
+        val addOne: Int => Int = _ + 1
+        println(addOne(5))
+        println("--------------------")
+
+        val weirdSum: (Int, Int) => Int = (_ * 2 + _ * 3)
+        println(weirdSum(2, 3))
         println("--------------------")
     }
 
@@ -18,5 +35,9 @@ object HelloWorld {
         x2 + y2
     }
 
-    def sumOfSquaresShort (x: Int, y: Int): Int = x * x + y * y
+    def sumOfSquaresShort(x: Int, y: Int): Int = x * x + y * y
+
+    def anonymousFunction(x: Int): Int = x * x
+
+    
 }
