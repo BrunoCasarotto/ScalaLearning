69a3b6785decffa554a36a49fc8610af0e00ffbd
BrunoCasarotto <bruno.casarotto@hotmail.com>
2025-04-04 21:57:21 -0300

    chore: leetcode 6

--- scala/leetcode/6_ZigzagConversion.scala
+++ scala/leetcode/6_ZigzagConversion.scala
@@ -6,19 +6,19 @@ object Solution {
         val cycleLen = 2 * numRows - 2
         
         for (i <- 0 until s.length) {
-        val pos = i % cycleLen
-        val row = if (pos < numRows) pos else cycleLen - pos
-        charsPerRow(row) += 1
+            val pos = i % cycleLen
+            val row = if (pos < numRows) pos else cycleLen - pos
+            charsPerRow(row) += 1
         }
         
         val rows = Array.tabulate(numRows)(i => new StringBuilder(charsPerRow(i)))
         
         var index = 0
         while (index < s.length) {
-        val pos = index % cycleLen
-        val row = if (pos < numRows) pos else cycleLen - pos
-        rows(row).append(s(index))
-        index += 1
+            val pos = index % cycleLen
+            val row = if (pos < numRows) pos else cycleLen - pos
+            rows(row).append(s(index))
+            index += 1
         }
 
         val result = new StringBuilder(s.length)
