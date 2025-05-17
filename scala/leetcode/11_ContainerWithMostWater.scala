4715947a14000ad047db12d7fca11096fada4d95
BrunoCasarotto <bruno.casarotto@hotmail.com>
2025-05-17 15:00:30 -0300

    chore: leetcode 11

--- scala/leetcode/11_ContainerWithMostWater.scala
+++ scala/leetcode/11_ContainerWithMostWater.scala
@@ -1,5 +1,29 @@
 object Solution {
     def maxArea(height: Array[Int]): Int = {
+        var left = 0
+        var right = height.length - 1
+        var maxWater = 0
         
+        while (left < right) {
+            // Calculate the current area
+            // Width = right - left
+            // Height = min(height[left], height[right])
+            val currentArea = (right - left) * Math.min(height(left), height(right))
+            
+            // Update the maximum area if current is larger
+            maxWater = Math.max(maxWater, currentArea)
+            
+            // Move the pointer that has the smaller height
+            // This is optimal because:
+            // 1. Moving the taller pointer would definitely decrease the height
+            // 2. Moving the shorter pointer might increase the height
+            if (height(left) < height(right)) {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+        
+        maxWater
     }
 }
